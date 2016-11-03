a1b550a90aaf6b8fed1ed691ae203c864d304ccb
123 <123@qq.com>
2016-11-03 21:40:04 +0800

    第一次提交

---
+++ app/src/main/java/com/example/wlw/qqlogin/Data.java
@@ -0,0 +1,13 @@
+package com.example.wlw.qqlogin;
+
+/**
+ * Created by wlw on 2016/10/31.
+ */
+public final class Data {
+
+    public static final String[][] USER={
+            {"1001","111","0"},
+            {"1002","111","1"},
+            {"1003","111","2"}
+    };
+}
